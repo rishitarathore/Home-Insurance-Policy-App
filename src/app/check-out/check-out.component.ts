16a9a2103c3aa8524062d0ab36d13384a55ee733
ksaidivya@teksystems.com <saidivya4654@gmail.com>
2021-10-26 10:59:47 +0530

    checkout-commit

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./check-out.component.css']
 })
 export class CheckOutComponent implements OnInit {
+  public textShow1 =false;
+  public textShow2 =false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  yes(){
+    this.textShow2 = false;
+   this.textShow1 = true;
+  }
+  no(){
+    this.textShow1 = false;
+   this.textShow2 = true;
+  }
 }
